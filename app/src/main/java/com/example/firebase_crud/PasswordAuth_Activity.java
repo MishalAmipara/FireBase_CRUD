a7a4df327fd4c393d114ff7becad3ad0a6cb336b
05 <mishal.amipara@gmail.com>
2023-05-04 15:33:59 +0530

    First Commit

---
+++ app/src/main/java/com/example/firebase_crud/PasswordAuth_Activity.java
@@ -0,0 +1,16 @@
+package com.example.firebase_crud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasswordAuth_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_auth);
+
+
+    }
+}
